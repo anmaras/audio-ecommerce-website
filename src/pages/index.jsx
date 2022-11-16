a8a45530c973a0d8cf0e91db20fc861f08fd1956
Anthony Maras <marasantonis@gmail.com>
2022-11-16 16:15:33 +0200

    initial commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,7 @@
+import HomePage from './HomePage';
+import CheckOutPage from './CheckOutPage';
+import ProductsPage from './ProductsPage';
+import SingleProductPage from './SingleProductPage';
+import ErrorPage from './ErrorPage';
+
+export { HomePage, CheckOutPage, ProductsPage, SingleProductPage, ErrorPage };
---
+++ src/utils/constants.jsx
@@ -0,0 +1,24 @@
+const links = [
+  {
+    id: 1,
+    text: 'home',
+    url: '/',
+  },
+  {
+    id: 2,
+    text: 'headphones',
+    url: '/products/headphones',
+  },
+  {
+    id: 3,
+    text: 'speakers',
+    url: '/products/speakers',
+  },
+  {
+    id: 4,
+    text: 'earphones',
+    url: '/products/earphones',
+  },
+];
+
+export default links;
