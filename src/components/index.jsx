baca7114de92339dc8bebe743459e6e241f6c5da
Anthony Maras <marasantonis@gmail.com>
2022-11-17 20:49:08 +0200

    update index

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,4 +1,6 @@
 import Navbar from './Navbar';
 import Menu from './Menu';
+import MenuList from './MenuList';
+import NewProduct from './NewProduct';
 
-export { Navbar, Menu };
+export { Navbar, Menu, MenuList, NewProduct };
