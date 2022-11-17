63e66e7ea75965c6fe889345ca70be582a615b18
Anthony Maras <marasantonis@gmail.com>
2022-11-17 15:38:28 +0200

    add product.provider

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { ProductsProvider } from './context/products_context';
 import './styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <ProductsProvider>
+      <App />
+    </ProductsProvider>
   </React.StrictMode>
 );
