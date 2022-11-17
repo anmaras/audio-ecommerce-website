55efd5a7ab041355810ef4711b573e9abe49675c
Anthony Maras <marasantonis@gmail.com>
2022-11-17 15:38:50 +0200

    update constants obj properties

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -8,16 +8,25 @@ const links = [
     id: 2,
     text: 'headphones',
     url: '/products/headphones',
+    thumbnail:
+      '/src/assets/shared/desktop/image-category-thumbnail-headphones.png',
+    category: 'headphones',
   },
   {
     id: 3,
     text: 'speakers',
     url: '/products/speakers',
+    thumbnail:
+      '/src/assets/shared/desktop/image-category-thumbnail-speakers.png',
+    category: 'speakers',
   },
   {
     id: 4,
     text: 'earphones',
     url: '/products/earphones',
+    thumbnail:
+      '/src/assets/shared/desktop/image-category-thumbnail-earphones.png',
+    category: 'earphones',
   },
 ];
 
