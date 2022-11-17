603054971321da76564bce5f42fb351ec7f58626
Anthony Maras <marasantonis@gmail.com>
2022-11-17 15:40:10 +0200

    setup menu jsx

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,7 +1,44 @@
 import React from 'react';
+import links from '../utils/constants';
+import { Link } from 'react-router-dom';
+import buttonImgUrl from '../assets/shared/desktop/icon-arrow-right.svg';
+import { useProductsContext } from '../context/products_context';
 
 const Menu = () => {
-  return <div>Menu</div>;
+  const { isMenuOpen, closeMenu } = useProductsContext();
+
+  return (
+    <>
+      <div
+        className={isMenuOpen ? 'backdrop backdrop--visible' : 'backdrop'}
+        onClick={closeMenu}
+      ></div>
+      <div className={isMenuOpen ? 'menu menu--visible' : 'menu'}>
+        <ul className="menu__linksList">
+          {links.map((link, index) => {
+            const { id, url, category, thumbnail } = link;
+            if (index > 0)
+              return (
+                <li key={id} className="menu__link">
+                  <Link className="router-link" to={url} onClick={closeMenu}>
+                    <img className="menu__thumbnail" src={thumbnail} alt="" />
+                    <p className="menu__title">{category}</p>
+                    <button className="menu__button button-3">
+                      shop
+                      <img
+                        className="button-3__icon"
+                        src={buttonImgUrl}
+                        alt=""
+                      />
+                    </button>
+                  </Link>
+                </li>
+              );
+          })}
+        </ul>
+      </div>
+    </>
+  );
 };
 
 export default Menu;
