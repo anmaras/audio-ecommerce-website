1578fef56f642efc676598671427284ff036f0e2
Anthony Maras <marasantonis@gmail.com>
2022-11-17 15:39:21 +0200

    add actions for menu and load data from firebase

--- src/reducers/product_reducer.jsx
+++ src/reducers/product_reducer.jsx
@@ -1,4 +1,29 @@
+import {
+  TOGGLE_MENU,
+  CLOSE_MENU,
+  GET_PRODUCTS_BEGIN,
+  GET_PRODUCTS_SUCCESS,
+  GET_PRODUCTS_ERROR,
+} from '../actions/actions';
+
 const product_reducer = (state, action) => {
+  if (action.type === TOGGLE_MENU) {
+    return { ...state, isMenuOpen: !state.isMenuOpen };
+  }
+  if (action.type === CLOSE_MENU) {
+    return { ...state, isMenuOpen: false };
+  }
+
+  if (action.type === GET_PRODUCTS_SUCCESS) {
+    /* function to filter only the main categories from the data array */
+    const previewProducts = Array.from(
+      new Set(action.payload.map((item) => item.category))
+    ).map((category) => {
+      return action.payload.find((item) => item.category === category);
+    });
+
+    return { ...state, products: action.payload, previewProducts };
+  }
   throw new Error(`No Matching "${action.type}" - action type`);
 };
 
