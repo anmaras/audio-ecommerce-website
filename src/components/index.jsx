95a640b74b0cfa5312fe2818fe24fec9a9138580
Anthony Maras <marasantonis@gmail.com>
2022-11-18 19:27:31 +0200

    update index

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -2,5 +2,18 @@ import Navbar from './Navbar';
 import Menu from './Menu';
 import MenuList from './MenuList';
 import NewProduct from './NewProduct';
+import About from './About';
+import CategoriesList from './CategoriesList';
+import Logo from './Logo';
+import Footer from './Footer';
 
-export { Navbar, Menu, MenuList, NewProduct };
+export {
+  Navbar,
+  Menu,
+  MenuList,
+  NewProduct,
+  About,
+  CategoriesList,
+  Logo,
+  Footer,
+};
