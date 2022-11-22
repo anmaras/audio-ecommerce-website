a6d185c29210af8cd599b47fb740b3d706734e54
Anthony Maras <marasantonis@gmail.com>
2022-11-22 12:46:31 +0200

    update jsx

--- src/components/MenuList.jsx
+++ src/components/MenuList.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import links from '../utils/constants';
+import { links } from '../utils/constants';
 import buttonImgUrl from '../assets/shared/desktop/icon-arrow-right.svg';
 import { useProductsContext } from '../context/products_context';
 
 const MenuList = () => {
-  const { closeMenu } = useProductsContext();
+  const { closeMenu, getProductsByCategory } = useProductsContext();
   return (
-    <ul className="menu__linksList">
+    <ul className="menuList">
       {links.map((link, index) => {
         const { id, url, category, thumbnail } = link;
         if (index > 0)
           return (
-            <li key={id} className="menu__link">
-              <Link className="router-link" to={url} onClick={closeMenu}>
-                <img className="menu__thumbnail" src={thumbnail} alt="" />
-                <p className="menu__title">{category}</p>
-                <button className="menu__button button-3">
+            <li key={id} className="menuList__link">
+              <Link
+                className="router-link"
+                to={url}
+                onClick={() => {
+                  getProductsByCategory(category);
+                  closeMenu();
+                }}
+              >
+                <img className="menuList__thumbnail" src={thumbnail} alt="" />
+                <p className="menuList__title">{category}</p>
+                <button className="menuList__button button-3">
                   shop
                   <img className="button-3__icon" src={buttonImgUrl} alt="" />
                 </button>
