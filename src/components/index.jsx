ae0fe3c9fc2bee44c0ac4f5c8f05d951be9ad32e
Anthony Maras <marasantonis@gmail.com>
2022-11-22 12:37:45 +0200

    update index

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,19 +1,27 @@
 import Navbar from './Navbar';
-import Menu from './Menu';
+import NavbarMenu from './NavbarMenu';
 import MenuList from './MenuList';
 import NewProduct from './NewProduct';
 import About from './About';
-import CategoriesList from './CategoriesList';
+import NavbarLinks from './NavbarLinks';
 import Logo from './Logo';
 import Footer from './Footer';
+import ProductsList from './ProductsList';
+import PageHero from './PageHero';
+import Header from './Header';
+import Backdrop from './Backdrop';
 
 export {
   Navbar,
-  Menu,
+  NavbarMenu,
   MenuList,
   NewProduct,
   About,
-  CategoriesList,
+  NavbarLinks,
   Logo,
   Footer,
+  ProductsList,
+  PageHero,
+  Header,
+  Backdrop,
 };
