27cf8ac7040dbc6b8e85c716eb8b68ae4658acf2
Anthony Maras <marasantonis@gmail.com>
2022-11-24 19:30:40 +0200

    update component

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -3,44 +3,49 @@ import { Link } from 'react-router-dom';
 import { useProductsContext } from '../context/products_context';
 import { Pictures, Amount } from '../components/index';
 import style from '../styles/components/Product.module.scss';
-import { formatPrice } from '../utils/helpers';
-const { main, main__title, main__description, main__price } = style;
+import { formatPrice, splitText } from '../utils/helpers';
 
 const Product = () => {
-  const { single_product: product, products } = useProductsContext();
-
-  const desktop = `/src${product?.image?.desktop.substring(1)}`;
-  const tablet = `/src${product?.image?.tablet.substring(1)}`;
-  const mobile = `/src${product?.image?.mobile.substring(1)}`;
+  const { single_product: product } = useProductsContext();
 
   return (
     <>
+      <div className={style.button}>
+        <Link to={`/products/${product?.category}`}>go back</Link>
+      </div>
       <section className={style.product}>
-        <div className={style.button}>
-          <Link to={`/products/${product?.category}`}>go back</Link>
-        </div>
-        <picture className={style['product__picture']}>
-          <source media="(min-width:1440px)" srcSet={desktop} />
-          <source media="(min-width:768px)" srcSet={tablet} />
-          <source media="(min-width:375px)" srcSet={mobile} />
-          <img src={mobile} loading="lazy" alt={`${product?.name}`} />
-        </picture>
-        <p className="overline">new product</p>
-
-        <h2 className={style['product__title']}>{product?.name}</h2>
-        <p className={style['product__description']}>{product?.description}</p>
-        <p className={style['product__price']}>{formatPrice(product?.price)}</p>
-        <div className={style['product__wrapper-btn']}>
-          <Amount />
-          <button type="button" className="button-1">
-            add to cart
-          </button>
+        <Pictures
+          desktop={product?.image?.desktop}
+          tablet={product?.image?.tablet}
+          mobile={product?.image?.mobile}
+          alt={`${product?.name} image`}
+        />
+        <div className={style['product__textWrapper']}>
+          <p className="overline">new product</p>
+          <h2 className={style['product__title']}>{product?.name}</h2>
+          <p className={style['product__description']}>
+            {product?.description}
+          </p>
+          <p className={style['product__price']}>
+            {formatPrice(product?.price)}
+          </p>
+          <div className={style['product__wrapper-btn']}>
+            <Amount />
+            <button type="button" className="button-1">
+              add to cart
+            </button>
+          </div>
         </div>
       </section>
+
+      {/* features */}
+
       <section className={style.features}>
         <div className={style['features__textWrapper']}>
           <h3 className={style['features__title']}>features</h3>
-          <p className={style['features__description']}>{product?.features}</p>
+          <div className={style['features__description']}>
+            {splitText(product?.features)}
+          </div>
         </div>
         <div className={style['features__insidesWrapper']}>
           <h3 className={style['features__title']}>in the box</h3>
@@ -58,7 +63,10 @@ const Product = () => {
           </ul>
         </div>
       </section>
-      <section>
+
+      {/* gallery */}
+
+      <section className={style.gallery}>
         <Pictures
           desktop={product?.gallery?.first?.desktop}
           tablet={product?.gallery?.first?.tablet}
@@ -75,17 +83,20 @@ const Product = () => {
           mobile={product?.gallery?.third?.mobile}
         />
       </section>
-      <section>
-        <h2>you may also like</h2>
-        <ul>
+      <section className={style.alternatives}>
+        <h2 className={style['alternatives__title']}>you may also like</h2>
+        <ul className={style['alternatives__list']}>
           {product?.others?.map((other, index) => {
             const { desktop, tablet, mobile } = other?.image;
             const { name, slug } = other;
             return (
-              <li key={index}>
+              <li className={style['alternatives__item']} key={index}>
                 <Pictures desktop={desktop} tablet={tablet} mobile={mobile} />
                 <h5>{name}</h5>
-                <Link to={`/products/${product.category}/${slug}`}>
+                <Link
+                  to={`/products/${product.category}/${slug}`}
+                  className="button-1"
+                >
                   see product
                 </Link>
               </li>
