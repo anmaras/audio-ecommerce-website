344c6789e985242734b8af84ac376c444b7b2844
Anthony Maras <marasantonis@gmail.com>
2022-11-25 20:24:45 +0200

    import about component

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
-import { ProductsList, PageHero, MenuList } from '../components/index';
+import { ProductsList, PageHero, MenuList, About } from '../components/index';
 import { useProductsContext } from '../context/products_context';
 
 export default function ProductsPage() {
@@ -16,6 +16,7 @@ export default function ProductsPage() {
       <PageHero category={category} />
       <ProductsList />
       <MenuList />
+      <About />
     </main>
   );
 }
