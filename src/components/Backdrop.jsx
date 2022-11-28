bddba9d7cd9e8fd74f1f377afb4c85154bc1a8d3
Anthony Maras <marasantonis@gmail.com>
2022-11-28 18:06:06 +0200

    update component

--- src/components/Backdrop.jsx
+++ src/components/Backdrop.jsx
@@ -4,7 +4,14 @@ import { useProductsContext } from '../context/products_context';
 const Backdrop = () => {
   const { closeMenu } = useProductsContext();
 
-  return <div className="backdrop" onClick={closeMenu}></div>;
+  return (
+    <div
+      className="backdrop"
+      onClick={() => {
+        closeMenu();
+      }}
+    ></div>
+  );
 };
 
 export default Backdrop;
