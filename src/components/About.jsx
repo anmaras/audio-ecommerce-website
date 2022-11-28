ad19c0550a63901c0ab4f8ac36a1fc837216b3d4
Anthony Maras <marasantonis@gmail.com>
2022-11-28 17:58:15 +0200

    add picture component and module style

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,18 +2,24 @@ import React from 'react';
 import mobile from '../assets/shared/mobile/image-best-gear.jpg';
 import tablet from '../assets/shared/tablet/image-best-gear.jpg';
 import desktop from '../assets/shared/desktop/image-best-gear.jpg';
-import { Images } from '../components/index';
-const images = { desktop, tablet, mobile };
+import { Pictures } from '../components/index';
+import style from '../styles/components/About.module.scss';
 
 const About = () => {
   return (
-    <section className="about">
-      <Images images={images} alt={'man listening to music with headphones'} />
-      <div className="about__text-wrapper">
-        <h2 className="about__title">
-          bringing you the <span className="about__span">best</span> audio gear
+    <section className={style.about}>
+      <Pictures
+        desktop={desktop}
+        tablet={tablet}
+        mobile={mobile}
+        alt={'man listening to music with headphones'}
+      />
+      <div className={style['about__text-wrapper']}>
+        <h2 className={style['about__title']}>
+          bringing you the <span className={style['about__span']}>best</span>{' '}
+          audio gear
         </h2>
-        <p className="about__text">
+        <p className={style['about__text']}>
           Located at the heart of New York City, Audiophile is the premier store
           for high end headphones, earphones, speakers, and audio accessories.
           We have a large showroom and luxury demonstration rooms available for
