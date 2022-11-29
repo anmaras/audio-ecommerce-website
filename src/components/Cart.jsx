2767ab5ee69e407f40282616b315f81c27256d1d
Anthony Maras <marasantonis@gmail.com>
2022-11-29 13:43:48 +0200

    add backdrop

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import style from '../styles/components/Cart.module.scss';
 import img1 from '../assets/cart/image-xx99-mark-two-headphones.jpg';
-import { Amount, Backdrop } from './index';
+import { Amount } from './index';
 import { Link } from 'react-router-dom';
 import { useProductsContext } from '../context/products_context';
 
 const Cart = () => {
-  const { isCartOpen } = useProductsContext();
+  const { isCartOpen, toggleCart, closeCart } = useProductsContext();
 
   if (isCartOpen) {
     return (
       <>
         <section className={style.cart}>
+          <div className={style.backdrop} onClick={closeCart}></div>
           <article className={style['cart__wrapper']}>
             <section className={style['cart__header']}>
               <p>cart (3)</p>
@@ -71,12 +72,11 @@ const Cart = () => {
               <p>total</p>
               <p>$ 5,323</p>
             </div>
-            <Link to={'/checkout'} className="button-1">
+            <Link to={'/checkout'} className="button-1" onClick={toggleCart}>
               checkout
             </Link>
           </article>
         </section>
-        <div className={style.backdrop}></div>
       </>
     );
   }
