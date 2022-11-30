b346945aa6059406050be47b72ba426bed7b2906
Anthony Maras <marasantonis@gmail.com>
2022-11-30 14:26:26 +0200

    set up cart list

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -4,9 +4,13 @@ import img1 from '../assets/cart/image-xx99-mark-two-headphones.jpg';
 import { Amount } from './index';
 import { Link } from 'react-router-dom';
 import { useProductsContext } from '../context/products_context';
+import { useCartContext } from '../context/cart_context';
+import { formatPrice, formatName } from '../utils/helpers';
+import uuid from 'react-uuid';
 
 const Cart = () => {
   const { isCartOpen, toggleCart, closeCart } = useProductsContext();
+  const { cart, clearCart, toggleAmount } = useCartContext();
 
   if (isCartOpen) {
     return (
@@ -15,58 +19,29 @@ const Cart = () => {
           <div className={style.backdrop} onClick={closeCart}></div>
           <article className={style['cart__wrapper']}>
             <section className={style['cart__header']}>
-              <p>cart (3)</p>
-              <button type="button">remove all</button>
+              <p>cart ({cart.length})</p>
+              <button type="button" onClick={clearCart}>
+                remove all
+              </button>
             </section>
             <ul className={style['cart__list']}>
-              <li className={style['cart__item']}>
-                <img src={img1} alt="" />
-                <div>
-                  <p>xx99 mk ii</p>
-                  <p>$ 2,999</p>
-                </div>
-                <Amount />
-              </li>
-              <li className={style['cart__item']}>
-                <img src={img1} alt="" />
-                <div>
-                  <p>xx99 mk ii</p>
-                  <p>$ 2,999</p>
-                </div>
-                <Amount />
-              </li>
-              <li className={style['cart__item']}>
-                <img src={img1} alt="" />
-                <div>
-                  <p>xx99 mk ii</p>
-                  <p>$ 2,999</p>
-                </div>
-                <Amount />
-              </li>
-              <li className={style['cart__item']}>
-                <img src={img1} alt="" />
-                <div>
-                  <p>xx99 mk ii</p>
-                  <p>$ 2,999</p>
-                </div>
-                <Amount />
-              </li>
-              <li className={style['cart__item']}>
-                <img src={img1} alt="" />
-                <div>
-                  <p>xx99 mk ii</p>
-                  <p>$ 2,999</p>
-                </div>
-                <Amount />
-              </li>
-              <li className={style['cart__item']}>
-                <img src={img1} alt="" />
-                <div>
-                  <p>xx99 mk ii</p>
-                  <p>$ 2,999</p>
-                </div>
-                <Amount />
-              </li>
+              {cart.map((item) => {
+                const { id, name, image, price, amount } = item;
+                return (
+                  <li key={uuid()} className={style['cart__item']}>
+                    <img src={image} alt={name} />
+                    <div>
+                      <p>{formatName(name)}</p>
+                      <p>{formatPrice(price)}</p>
+                    </div>
+                    <Amount
+                      amount={amount}
+                      increase={() => toggleAmount(id, 'inc')}
+                      decrease={() => toggleAmount(id, 'dec')}
+                    />
+                  </li>
+                );
+              })}
             </ul>
             <div className={style['cart__total']}>
               <p>total</p>
