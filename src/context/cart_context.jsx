469ae4225e00b4dfa17576fff508561d57e9568e
Anthony Maras <marasantonis@gmail.com>
2022-11-30 13:17:10 +0200

    set up cart context

---
+++ src/context/cart_context.jsx
@@ -0,0 +1,54 @@
+import React, { useContext, useEffect, useReducer } from 'react';
+import reducer from '../reducers/cart_reducer';
+
+import {
+  ADD_TO_CART,
+  CLEAR_CART,
+  TOGGLE_CART_ITEM_AMOUNT,
+  REMOVE_CART_ITEM,
+  COUNT_CART_TOTALS,
+} from '../actions/actions';
+
+const getLocalStorage = () => {
+  let cart = localStorage.getItem('cart');
+  return cart ? JSON.parse(localStorage.getItem('cart')) : [];
+};
+
+const initialState = {
+  cart: [],
+  total_items: 0,
+  total_amount: 0,
+  shipping_fee: 50,
+};
+
+const CartContext = React.createContext();
+
+export const CartProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  const addToCart = (product, amount, id) => {
+    dispatch({ type: ADD_TO_CART, payload: { product, amount, id } });
+  };
+
+  console.log(state);
+
+  // useEffect(() => {
+  //   dispatch({ type: COUNT_CART_TOTALS });
+  //   localStorage.setItem('cart', JSON.stringify(state.cart));
+  // }, [state.cart]);
+
+  return (
+    <CartContext.Provider
+      value={{
+        ...state,
+        addToCart,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export const useCartContext = () => {
+  return useContext(CartContext);
+};
