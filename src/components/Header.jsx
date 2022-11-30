492a121a6352e7d851a8e158572f6e2c26e6cdd3
Anthony Maras <marasantonis@gmail.com>
2022-11-30 16:40:09 +0200

    add cart amount on cart img

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import hamburgerUrl from '../assets/shared/tablet/icon-hamburger.svg';
 import cartUrl from '../assets/shared/desktop/icon-cart.svg';
 import { useProductsContext } from '../context/products_context';
+import { useCartContext } from '../context/cart_context';
 import { NavbarLinks, Logo } from './index';
 import style from '../styles/components/Header.module.scss';
 
 const Header = () => {
   const { toggleMenu, toggleCart } = useProductsContext();
+  const { total_items: total } = useCartContext();
 
   return (
     <header className={style.header}>
@@ -20,13 +22,18 @@ const Header = () => {
         />
         <Logo className={style['header__log']} />
         <NavbarLinks className={style['header__linksList']} />
-        <img
-          src={cartUrl}
-          alt="cart icon"
-          className={style['header__cart']}
-          loading="lazy"
-          onClick={toggleCart}
-        />
+        <div className={style['header__cartWrapper']}>
+          <img
+            src={cartUrl}
+            alt="cart icon"
+            className={style['header__cart']}
+            loading="lazy"
+            onClick={toggleCart}
+          />
+          <div className={style['header__amount']}>
+            <p>{total}</p>
+          </div>
+        </div>
       </nav>
     </header>
   );
