ead1a2c349c16f2f6c2236f346069cb092813c5f
Anthony Maras <marasantonis@gmail.com>
2022-11-30 14:27:45 +0200

    add toggleAmount func

--- src/reducers/cart_reducer.jsx
+++ src/reducers/cart_reducer.jsx
@@ -2,7 +2,6 @@ import {
   ADD_TO_CART,
   CLEAR_CART,
   TOGGLE_CART_ITEM_AMOUNT,
-  REMOVE_CART_ITEM,
   COUNT_CART_TOTALS,
 } from '../actions/actions';
 
@@ -11,10 +10,7 @@ const cart_reducer = (state, action) => {
     const { amount, product, id } = action.payload;
     /* find the item  */
     const tempItem = state.cart.find((item) => item.id === id);
-    console.log(tempItem);
-
     /* if item exist in cart */
-
     if (tempItem) {
       /* return a new array if item exist and update the amount */
       const tempCart = state.cart.map((item) => {
@@ -37,9 +33,36 @@ const cart_reducer = (state, action) => {
         name: product.name,
         image: `/src/assets/cart/image-${product.slug}.jpg`,
       };
-
       return { ...state, cart: [...state.cart, newItem] };
     }
+  }
+
+  if (action.type === TOGGLE_CART_ITEM_AMOUNT) {
+    const { id, value } = action.payload;
+    /* make a temp array and depend the value add or remove from the 
+    cart item amount  */
+    const tempCart = state.cart
+      .map((item) => {
+        if (item.id === id) {
+          if (value === 'inc') {
+            let newAmount = item.amount + 1;
+
+            return { ...item, amount: newAmount };
+          }
+          if (value === 'dec') {
+            let newAmount = item.amount - 1;
+
+            return { ...item, amount: newAmount };
+          }
+        }
+        return item;
+      }) // if amount is lower than 1 remove it from the cart
+      .filter((cartItem) => cartItem.amount !== 0);
+    return { ...state, cart: tempCart };
+  }
+
+  if (action.type === CLEAR_CART) {
+    return { ...state, cart: [] };
   }
 
   throw new Error(`No Matching "${action.type}" - action type`);
