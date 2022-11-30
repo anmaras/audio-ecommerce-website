c84a828a5aff036fbe07fb7b117e7ac35622b66e
Anthony Maras <marasantonis@gmail.com>
2022-11-30 13:17:44 +0200

    add cart provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ProductsProvider } from './context/products_context';
+import { CartProvider } from './context/cart_context';
 import './styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ProductsProvider>
-      <App />
+      <CartProvider>
+        <App />
+      </CartProvider>
     </ProductsProvider>
   </React.StrictMode>
 );
