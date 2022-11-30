ce4597d2d7fd396a32808391682a64d5917bfdee
Anthony Maras <marasantonis@gmail.com>
2022-11-30 13:15:52 +0200

    set up props for component

--- src/components/Amount.jsx
+++ src/components/Amount.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import style from '../styles/components/Amount.module.scss';
 
-const Amount = () => {
+const Amount = ({ amount, increase, decrease }) => {
   return (
     <div className={style.amount}>
-      <button type="button" className={style['amount__decrease']}>
+      <button
+        type="button"
+        className={style['amount__decrease']}
+        onClick={decrease}
+      >
         -
       </button>
-      <p className="amount__number">1</p>
-      <button type="button" className={style['amount__increase']}>
+      <p className="amount__number">{amount}</p>
+      <button
+        type="button"
+        className={style['amount__increase']}
+        onClick={increase}
+      >
         +
       </button>
     </div>
