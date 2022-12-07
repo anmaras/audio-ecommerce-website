5300bb0a4cee4af3ea53f2da041342b0051be8e7
Anthony Maras <marasantonis@gmail.com>
2022-12-07 18:56:48 +0200

    add redirect to last item of cart

--- src/pages/CheckOutPage.jsx
+++ src/pages/CheckOutPage.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import { GoBackButton, FormContainer } from '../components';
-import { useProductsContext } from '../context/products_context';
 import { useCartContext } from '../context/cart_context';
 
 export default function CheckOutPage() {
-  const { single_product: product } = useProductsContext();
   const { cart } = useCartContext();
+  const { category, slug } = cart[cart.length - 1];
   /* to redirect when page reload use the last item from cart array */
   return (
     <main className="main checkout">
-      <GoBackButton
-        category={`/products/${product?.category}/${product?.slug}`}
-      />
+      <GoBackButton category={`/products/${category}/${slug}`} />
       <section className="main__checkoutPageWrapper">
         <FormContainer />
       </section>
