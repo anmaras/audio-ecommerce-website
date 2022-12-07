2f9d9097a685f46977dcae562a5f7da37bcf6b33
Anthony Maras <marasantonis@gmail.com>
2022-12-07 21:57:24 +0200

    add scroll to top on nav links

--- src/components/NavbarLinks.jsx
+++ src/components/NavbarLinks.jsx
@@ -5,14 +5,19 @@ import { useProductsContext } from '../context/products_context';
 
 const CategoriesList = ({ className }) => {
   const { getProductsByCategory } = useProductsContext();
-
   return (
     <ul className={className ? className : 'list'}>
       {links.map((link) => {
         const { id, text, url } = link;
         return (
           <li key={id} className="list__link">
-            <Link to={url} onClick={() => getProductsByCategory(text)}>
+            <Link
+              to={url}
+              onClick={() => {
+                getProductsByCategory(text);
+                window.scrollTo({ top: 0, behavior: 'smooth' });
+              }}
+            >
               {text}
             </Link>
           </li>
