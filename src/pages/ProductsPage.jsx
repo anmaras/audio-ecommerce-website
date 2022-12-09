a3080b02ed72c2cd29d7a883b6e5ede8bd726325
Anthony Maras <marasantonis@gmail.com>
2022-12-09 21:27:37 +0200

    add loading and animation and remove useEffect

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -1,27 +1,37 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
 import {
   ProductsCategories,
   PageHero,
   MenuList,
   About,
+  Spinner,
 } from '../components/index';
 import { useProductsContext } from '../context/products_context';
+import { motion } from 'framer-motion';
+import { menuListVariant } from '../AnimationVariants/variants';
 
 export default function ProductsPage() {
   const { category } = useParams();
-  const { getProductsByCategory } = useProductsContext();
+  const { products_loading: loading } = useProductsContext();
 
-  useEffect(() => {
-    getProductsByCategory(category);
-  }, [category]);
+  if (loading) {
+    return <Spinner />;
+  }
 
   return (
     <main className="main categories">
       <PageHero category={category} />
       <section className="main__productsPageWrapper">
-        <ProductsCategories />
-        <MenuList />
+        <ProductsCategories category={category} />
+        <motion.div
+          variants={menuListVariant}
+          initial="initial"
+          whileInView="whileInView"
+          viewport={{ once: true }}
+        >
+          <MenuList />
+        </motion.div>
         <About />
       </section>
     </main>
