f8c4ab2ebd7dcaa3e564974e96b50f6d37860e44
Anthony Maras <marasantonis@gmail.com>
2022-12-09 21:27:53 +0200

    add loading and remove useEffect

--- src/pages/SingleProductPage.jsx
+++ src/pages/SingleProductPage.jsx
@@ -1,22 +1,20 @@
 import React from 'react';
-import { Product, About, GoBackButton } from '../components/index';
-import { useEffect } from 'react';
+import { Product, About, GoBackButton, Spinner } from '../components/index';
 import { useParams } from 'react-router-dom';
 import { useProductsContext } from '../context/products_context';
 
 export default function SingleProductPage() {
   const { id, category } = useParams();
-  const { fetchSingleProduct } = useProductsContext();
+  const { products_loading: loading } = useProductsContext();
 
-  useEffect(() => {
-    fetchSingleProduct(id);
-  }, [id]);
-
+  if (loading) {
+    return <Spinner />;
+  }
   return (
     <main className="main singleProduct">
       <GoBackButton category={`/products/${category}`} />
       <section className="main__productPageWrapper">
-        <Product />
+        <Product productId={id} />
         <About />
       </section>
     </main>
