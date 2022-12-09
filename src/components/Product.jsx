5b1cb100b5d42d28f36c4615757fc468bb3e56bf
Anthony Maras <marasantonis@gmail.com>
2022-12-09 21:26:45 +0200

    add animation

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,14 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { useProductsContext } from '../context/products_context';
-import { Pictures, Amount } from '../components/index';
+import { Pictures, Amount, Spinner } from '../components/index';
 import style from '../styles/components/Product.module.scss';
 import { formatPrice, splitText } from '../utils/helpers';
 import { useCartContext } from '../context/cart_context';
-const Product = () => {
-  const { single_product: product } = useProductsContext();
+import { motion } from 'framer-motion';
+import {
+  categoryImgVariant,
+  categoryText,
+  featuresTextVariant,
+  inTheBoxVariant,
+  featuresVariant,
+  imgGalleryVariant,
+  altVariant,
+} from '../AnimationVariants/variants';
+
+const Product = ({ productId }) => {
+  const {
+    single_product: product,
+    fetchSingleProduct,
+    products,
+  } = useProductsContext();
   const { addToCart } = useCartContext();
   const [amount, setAmount] = useState(1);
+
+  useEffect(() => {
+    fetchSingleProduct(productId);
+  }, [products]);
+
+  console.log(products);
 
   const increase = () => {
     setAmount((prev) => (prev === amount ? prev + 1 : amount));
@@ -21,13 +42,25 @@ const Product = () => {
   return (
     <>
       <section className={style.product}>
-        <Pictures
-          desktop={product?.image?.desktop}
-          tablet={product?.image?.tablet}
-          mobile={product?.image?.mobile}
-          alt={`${product?.name} image`}
-        />
-        <div className={style['product__textWrapper']}>
+        <motion.div
+          variants={categoryImgVariant}
+          initial="initial"
+          whileInView="whileInView"
+          viewport={{ once: true }}
+        >
+          <Pictures
+            desktop={product?.image?.desktop}
+            tablet={product?.image?.tablet}
+            mobile={product?.image?.mobile}
+            alt={`${product?.name} image`}
+          />
+        </motion.div>
+        <motion.div
+          className={style['product__textWrapper']}
+          variants={categoryText}
+          initial="initial"
+          animate="animate"
+        >
           <p className="overline">new product</p>
           <h2 className={style['product__title']}>{product?.name}</h2>
           <p className={style['product__description']}>
@@ -46,19 +79,30 @@ const Product = () => {
               add to cart
             </button>
           </div>
-        </div>
+        </motion.div>
       </section>
 
       {/* features */}
 
-      <section className={style.features}>
-        <div className={style['features__textWrapper']}>
+      <motion.section
+        className={style.features}
+        variants={featuresVariant}
+        initial="initial"
+        animate="animate"
+      >
+        <motion.div
+          className={style['features__textWrapper']}
+          variants={featuresTextVariant}
+        >
           <h3 className={style['features__title']}>features</h3>
           <div className={style['features__description']}>
             {splitText(product?.features)}
           </div>
-        </div>
-        <div className={style['features__insidesWrapper']}>
+        </motion.div>
+        <motion.div
+          className={style['features__insidesWrapper']}
+          variants={inTheBoxVariant}
+        >
           <h3 className={style['features__title']}>in the box</h3>
           <ul className={style['features__list']}>
             {product?.includes?.map((item, index) => {
@@ -72,49 +116,82 @@ const Product = () => {
               );
             })}
           </ul>
-        </div>
-      </section>
+        </motion.div>
+      </motion.section>
 
       {/* gallery */}
 
       <section className={style.gallery}>
-        <Pictures
-          desktop={product?.gallery?.first?.desktop}
-          tablet={product?.gallery?.first?.tablet}
-          mobile={product?.gallery?.first?.mobile}
-        />
-        <Pictures
-          desktop={product?.gallery?.second?.desktop}
-          tablet={product?.gallery?.second?.tablet}
-          mobile={product?.gallery?.second?.mobile}
-        />
-        <Pictures
-          desktop={product?.gallery?.third?.desktop}
-          tablet={product?.gallery?.third?.tablet}
-          mobile={product?.gallery?.third?.mobile}
-        />
+        <motion.div
+          variants={imgGalleryVariant}
+          initial="initial"
+          whileInView="whileInView"
+          viewport={{ once: true }}
+        >
+          <Pictures
+            desktop={product?.gallery?.first?.desktop}
+            tablet={product?.gallery?.first?.tablet}
+            mobile={product?.gallery?.first?.mobile}
+          />
+        </motion.div>
+        <motion.div
+          variants={imgGalleryVariant}
+          initial="initial"
+          whileInView="whileInView2"
+          viewport={{ once: true }}
+        >
+          <Pictures
+            desktop={product?.gallery?.second?.desktop}
+            tablet={product?.gallery?.second?.tablet}
+            mobile={product?.gallery?.second?.mobile}
+          />
+        </motion.div>
+        <motion.div
+          variants={imgGalleryVariant}
+          initial="initial"
+          whileInView="whileInView3"
+          viewport={{ once: true }}
+        >
+          <Pictures
+            desktop={product?.gallery?.third?.desktop}
+            tablet={product?.gallery?.third?.tablet}
+            mobile={product?.gallery?.third?.mobile}
+          />
+        </motion.div>
       </section>
 
       {/* alternatives */}
 
-      <section className={style.alternatives}>
+      <motion.section
+        className={style.alternatives}
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+      >
         <h2 className={style['alternatives__title']}>you may also like</h2>
         <ul className={style['alternatives__list']}>
           {product?.others?.map((other, index) => {
             const { desktop, tablet, mobile } = other?.image;
             const { name, slug, category } = other;
             return (
-              <li className={style['alternatives__item']} key={index}>
+              <motion.li
+                className={style['alternatives__item']}
+                key={index}
+                variants={altVariant}
+                initial="initial"
+                whileInView="whileInView"
+                transition={{ delay: index * 0.2 }}
+                viewport={{ once: true }}
+              >
                 <Pictures desktop={desktop} tablet={tablet} mobile={mobile} />
                 <h5>{name}</h5>
                 <Link to={`/products/${category}/${slug}`} className="button-1">
                   see product
                 </Link>
-              </li>
+              </motion.li>
             );
           })}
         </ul>
-      </section>
+      </motion.section>
     </>
   );
 };
--- src/components/ProductsCategories.jsx
+++ src/components/ProductsCategories.jsx
@@ -3,14 +3,25 @@ import { useProductsContext } from '../context/products_context';
 import { Link } from 'react-router-dom';
 import { Pictures } from './index';
 import style from '../styles/components/ProductsCategories.module.scss';
+import { motion } from 'framer-motion';
+import {
+  categoryImgVariant,
+  categoryText,
+} from '../AnimationVariants/variants';
+import { useEffect } from 'react';
 
-const ProductsList = () => {
-  const { category_products: products } = useProductsContext();
+const ProductsList = ({ category }) => {
+  const { category_products: products, getProductsByCategory } =
+    useProductsContext();
+
+  useEffect(() => {
+    getProductsByCategory(category);
+  }, [category]);
 
   return (
     <section className={style.categories}>
       <ul className={style['categories__list']}>
-        {products.map((product) => {
+        {products.map((product, index) => {
           const {
             categoryImage: { desktop, tablet, mobile },
             description,
@@ -22,13 +33,26 @@ const ProductsList = () => {
 
           return (
             <li className={style['categories__item']} key={id}>
-              <Pictures
-                desktop={desktop}
-                tablet={tablet}
-                mobile={mobile}
-                alt={`${name} picture`}
-              />
-              <div className={style['categories__wrapper']}>
+              <motion.div
+                variants={categoryImgVariant}
+                initial="initial"
+                whileInView="whileInView"
+                transition={{ delay: index * 0.2 }}
+                viewport={{ once: true }}
+              >
+                <Pictures
+                  desktop={desktop}
+                  tablet={tablet}
+                  mobile={mobile}
+                  alt={`${name} picture`}
+                />
+              </motion.div>
+              <motion.div
+                className={style['categories__wrapper']}
+                variants={categoryText}
+                initial="initial"
+                animate="animate"
+              >
                 <p className="overline">new product</p>
                 <h2 className={style['categories__title']}>{name}</h2>
                 <p className={style['categories__description']}>
@@ -37,7 +61,7 @@ const ProductsList = () => {
                 <Link className="button-1" to={`/products/${category}/${slug}`}>
                   see product
                 </Link>
-              </div>
+              </motion.div>
             </li>
           );
         })}
