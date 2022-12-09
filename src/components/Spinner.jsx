f1fbb3cbc58b00592d74a3583682283c1b400961
Anthony Maras <marasantonis@gmail.com>
2022-12-09 21:26:57 +0200

    remove animation

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,21 +1,15 @@
 import FadeLoader from 'react-spinners/FadeLoader';
 import style from '../styles/components/Spinner.module.scss';
 import { AnimatePresence } from 'framer-motion';
-import { motion } from 'framer-motion';
 
 import React from 'react';
 
 const Spinner = () => {
   return (
     <AnimatePresence>
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1, transition: { duration: 1 } }}
-        exit={{ opacity: 0 }}
-        className={style['spinner']}
-      >
-        <FadeLoader color="#da6f41" />;
-      </motion.div>
+      <div className={style['spinner']}>
+        <FadeLoader color="#da6f41" />
+      </div>
     </AnimatePresence>
   );
 };
