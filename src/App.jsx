90b2705d7da6d13c6dc677bf744639c54e005fa4
Anthony Maras <marasantonis@gmail.com>
2022-12-09 21:29:33 +0200

    clean up

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,15 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import { NavbarDropMenu, Footer, Header, Cart } from './components';
-import { useProductsContext } from './context/products_context';
 import AnimateRoutes from './AnimateRoutes';
-import Spinner from './components/Spinner';
 import ScrollToTop from './utils/ScrollToTop';
 
 function App() {
-  const { products_loading: loading } = useProductsContext();
-
-  if (loading) {
-    return <Spinner />;
-  }
-
   return (
     <Router>
       <ScrollToTop />
       <NavbarDropMenu />
       <Header />
       <Cart />
-      {/* {loading ? <Spinner /> : <AnimateRoutes />} */}
       <AnimateRoutes />
       <Footer />
     </Router>
