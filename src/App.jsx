4d549ed5c5f03835006b2c3bd1f92474d1dc2356
Anthony Maras <marasantonis@gmail.com>
2022-12-10 22:32:06 +0200

    remove scroll to top

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import { NavbarDropMenu, Footer, Header, Cart } from './components';
 import AnimateRoutes from './AnimateRoutes';
-import ScrollToTop from './utils/ScrollToTop';
 
 function App() {
   return (
     <Router>
-      <ScrollToTop />
       <NavbarDropMenu />
       <Header />
       <Cart />
