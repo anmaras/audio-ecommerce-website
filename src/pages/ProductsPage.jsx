ee72586ec0a53d058f767b42ffe687ac8b045bce
Anthony Maras <marasantonis@gmail.com>
2022-12-10 22:35:24 +0200

    add lazy motion

--- src/pages/ProductsPage.jsx
+++ src/pages/ProductsPage.jsx
@@ -8,32 +8,34 @@ import {
   Spinner,
 } from '../components/index';
 import { useProductsContext } from '../context/products_context';
-import { motion } from 'framer-motion';
+import { motion, LazyMotion, domAnimation, m } from 'framer-motion';
 import { menuListVariant } from '../AnimationVariants/variants';
 
 export default function ProductsPage() {
   const { category } = useParams();
-  const { products_loading: loading } = useProductsContext();
+  const { products_loading: loading, category_products: categories } =
+    useProductsContext();
 
   if (loading) {
     return <Spinner />;
   }
-
   return (
-    <main className="main categories">
-      <PageHero category={category} />
-      <section className="main__productsPageWrapper">
-        <ProductsCategories category={category} />
-        <motion.div
-          variants={menuListVariant}
-          initial="initial"
-          whileInView="whileInView"
-          viewport={{ once: true }}
-        >
-          <MenuList />
-        </motion.div>
-        <About />
-      </section>
-    </main>
+    <LazyMotion features={domAnimation}>
+      <main className="main categories">
+        <PageHero category={category} />
+        <section className="main__productsPageWrapper">
+          <ProductsCategories category={category} />
+          <motion.div
+            variants={menuListVariant}
+            initial="initial"
+            whileInView="whileInView"
+            viewport={{ once: true }}
+          >
+            <MenuList />
+          </motion.div>
+          <About />
+        </section>
+      </main>
+    </LazyMotion>
   );
 }
