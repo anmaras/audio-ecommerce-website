77e48b91ef9b274026e88c810e5140cedfde2c1f
Anthony Maras <marasantonis@gmail.com>
2022-12-10 22:29:37 +0200

    change img paths

--- src/utils/constants.jsx
+++ src/utils/constants.jsx
@@ -8,24 +8,21 @@ const links = [
     id: 2,
     text: 'headphones',
     url: '/products/headphones',
-    thumbnail:
-      '/src/assets/shared/desktop/image-category-thumbnail-headphones.png',
+    thumbnail: '/assets/shared/desktop/image-category-thumbnail-headphones.png',
     category: 'headphones',
   },
   {
     id: 3,
     text: 'speakers',
     url: '/products/speakers',
-    thumbnail:
-      '/src/assets/shared/desktop/image-category-thumbnail-speakers.png',
+    thumbnail: '/assets/shared/desktop/image-category-thumbnail-speakers.png',
     category: 'speakers',
   },
   {
     id: 4,
     text: 'earphones',
     url: '/products/earphones',
-    thumbnail:
-      '/src/assets/shared/desktop/image-category-thumbnail-earphones.png',
+    thumbnail: '/assets/shared/desktop/image-category-thumbnail-earphones.png',
     category: 'earphones',
   },
 ];
@@ -34,17 +31,17 @@ const socialLinks = [
   {
     id: 1,
     text: 'facebook',
-    img: '/src/assets/shared/desktop/icon-facebook.svg',
+    img: '/assets/shared/desktop/icon-facebook.svg',
   },
   {
     id: 2,
     text: 'twitter',
-    img: '/src/assets/shared/desktop/icon-twitter.svg',
+    img: '/assets/shared/desktop/icon-twitter.svg',
   },
   {
     id: 3,
     text: 'instagram',
-    img: '/src/assets/shared/desktop/icon-instagram.svg',
+    img: '/assets/shared/desktop/icon-instagram.svg',
   },
 ];
 
@@ -55,10 +52,10 @@ const previewProducts = [
     category: 'speakers',
     description:
       'Upgrade to premium speakers that are phenomenally built to deliver truly remarkable sound.',
-    mobile: '/src/assets/home/mobile/image-speaker-zx9.png',
-    tablet: '/src/assets/home/tablet/image-speaker-zx9.png',
-    desktop: '/src/assets/home/desktop/image-speaker-zx9.png',
-    decoration: '/src/assets/home/desktop/pattern-circles.svg',
+    mobile: '/assets/home/mobile/image-speaker-zx9.png',
+    tablet: '/assets/home/tablet/image-speaker-zx9.png',
+    desktop: '/assets/home/desktop/image-speaker-zx9.png',
+    decoration: '/assets/home/desktop/pattern-circles.svg',
     slug: 'zx9-speaker',
   },
   {
@@ -66,9 +63,9 @@ const previewProducts = [
     title: 'zx7',
     category: 'speakers',
     description: '',
-    mobile: '/src/assets/home/mobile/image-speaker-zx7.jpg',
-    tablet: '/src/assets/home/tablet/image-speaker-zx7.jpg',
-    desktop: '/src/assets/home/desktop/image-speaker-zx7.jpg',
+    mobile: '/assets/home/mobile/image-speaker-zx7.jpg',
+    tablet: '/assets/home/tablet/image-speaker-zx7.jpg',
+    desktop: '/assets/home/desktop/image-speaker-zx7.jpg',
     decoration: '',
     slug: 'zx7-speaker',
   },
@@ -77,9 +74,9 @@ const previewProducts = [
     title: 'yx1',
     category: 'earphones',
     description: '',
-    mobile: '/src/assets/home/mobile/image-earphones-yx1.jpg',
-    tablet: '/src/assets/home/tablet/image-earphones-yx1.jpg',
-    desktop: '/src/assets/home/desktop/image-earphones-yx1.jpg',
+    mobile: '/assets/home/mobile/image-earphones-yx1.jpg',
+    tablet: '/assets/home/tablet/image-earphones-yx1.jpg',
+    desktop: '/assets/home/desktop/image-earphones-yx1.jpg',
     decoration: '',
     slug: 'yx1-earphones',
   },
@@ -186,7 +183,4 @@ const inputs = [
 const productsUrl =
   'https://audio-data-fc218-default-rtdb.europe-west1.firebasedatabase.app/data.json';
 
-const productId =
-  'https://audio-data-fc218-default-rtdb.europe-west1.firebasedatabase.app/data';
-
-export { links, socialLinks, previewProducts, productsUrl, inputs, productId };
+export { links, socialLinks, previewProducts, productsUrl, inputs };
