1fb426b88c3ede32fdf863adc141cdba46b16397
Anthony Maras <marasantonis@gmail.com>
2022-12-11 16:02:42 +0200

    change path

--- src/components/MenuList.jsx
+++ src/components/MenuList.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { links } from '../utils/constants';
-import buttonImgUrl from '/assets/shared/desktop/icon-arrow-right.svg';
+import buttonImgUrl from '../../public/assets/shared/desktop/icon-arrow-right-svg';
 import { useProductsContext } from '../context/products_context';
 import style from '../styles/components/MenuList.module.scss';
 
