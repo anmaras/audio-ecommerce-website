82fef819009213358cf3b4fa6902f726367ab3cf
Anthony Maras <marasantonis@gmail.com>
2022-12-14 17:54:28 +0200

    update file

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import autoprefixer from 'autoprefixer';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
     postcss: {
-      plugins: [
-        autoprefixer({}), // add options if needed
-      ],
+      plugins: [autoprefixer({})],
     },
   },
   optimizeDeps: {
