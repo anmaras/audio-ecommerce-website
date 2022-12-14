33c65da97141b55395f4b6677b89c411b89b900f
Anthony Maras <marasantonis@gmail.com>
2022-12-14 17:46:21 +0200

    add animated menu icon

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,30 @@
 import React from 'react';
-import hamburgerUrl from '../assets/shared/tablet/icon-hamburger.svg';
 import cartUrl from '../assets/shared/desktop/icon-cart.svg';
 import { useMenuCartContext } from '../context/menu_cart_context';
 import { useCartContext } from '../context/cart_context';
 import { NavbarLinks, Logo } from './index';
+import { Spin as Hamburger } from 'hamburger-react';
 import style from '../styles/components/Header.module.scss';
 
 const Header = () => {
-  const { toggleMenu, toggleCart } = useMenuCartContext();
+  const { toggleMenu, toggleCart, closeCart, closeMenu, isMenuOpen } =
+    useMenuCartContext();
   const { total_items: total } = useCartContext();
 
   return (
     <header className={style.header}>
       <nav className={style['header__navbar']}>
-        <img
-          src={hamburgerUrl}
-          alt="hamburger menu icon"
-          className={style['header__hamburger']}
-          onClick={toggleMenu}
-          width="16"
-          height="15"
-        />
+        <div className={style['header__hamburger']}>
+          <Hamburger
+            toggle={toggleMenu}
+            toggled={isMenuOpen}
+            onToggle={closeCart}
+            className={style['header__hamburger']}
+            color="#ffffff"
+            distance="sm"
+            size={20}
+          />
+        </div>
         <Logo className={style['header__log']} />
         <NavbarLinks className={style['header__linksList']} />
         <div className={style['header__cartWrapper']}>
@@ -28,7 +32,10 @@ const Header = () => {
             src={cartUrl}
             alt="cart icon"
             className={style['header__cart']}
-            onClick={toggleCart}
+            onClick={() => {
+              toggleCart();
+              closeMenu();
+            }}
             width="23"
             height="20"
           />
