ab0ef9cb5bc3c80b4447953d89e9b66655338b78
Anthony Maras <marasantonis@gmail.com>
2022-12-14 17:52:33 +0200

    add condition for when the user clears the cart

--- src/pages/CheckOutPage.jsx
+++ src/pages/CheckOutPage.jsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import { useEffect } from 'react';
 import { GoBackButton, FormContainer } from '../components';
+import { useCartContext } from '../context/cart_context';
+import { useNavigate } from 'react-router-dom';
+import { useMenuCartContext } from '../context/menu_cart_context';
 
 export default function CheckOutPage() {
+  const { closeCart } = useMenuCartContext();
+  const { cart } = useCartContext();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (cart.length === 0) {
+      setTimeout(() => {
+        navigate('/');
+        setTimeout(() => {
+          closeCart();
+        }, 0);
+      }, 2000);
+    }
+  }, [cart.length]);
+
   return (
     <main className="main checkout">
       <GoBackButton />
