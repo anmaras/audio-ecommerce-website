8477292c06452447c9894df54a2343fc15cfd244
Anthony Maras <marasantonis@gmail.com>
2022-12-15 21:22:10 +0200

    add lazy and suspense

--- src/AnimateRoutes.jsx
+++ src/AnimateRoutes.jsx
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Routes, Route, useLocation } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
+import { Spinner } from './components';
+import { CheckOutPage } from './pages';
 
-import {
-  HomePage,
-  CheckOutPage,
-  ErrorPage,
-  ProductsPage,
-  SingleProductPage,
-} from './pages';
+const Home = lazy(() => import('./pages/HomePage'));
+const Categories = lazy(() => import('./pages/ProductsPage'));
+const Product = lazy(() => import('./pages/SingleProductPage'));
+// const CheckOut = lazy(() => import('./pages/CheckOutPage'));
+const Error = lazy(() => import('./pages/ErrorPage'));
 
 const AnimateRoutes = () => {
   const location = useLocation();
   return (
     <AnimatePresence>
-      <Routes location={location} key={location.pathname}>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/products/:category" element={<ProductsPage />} />
-        <Route path="/products/:category/:id" element={<SingleProductPage />} />
-        <Route path="/checkout" element={<CheckOutPage />} />
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
+      <Suspense fallback={<Spinner />}>
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Home />} />
+          <Route path="/products/:category" element={<Categories />} />
+          <Route path="/products/:category/:id" element={<Product />} />
+          <Route path="/checkout" element={<CheckOutPage />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Suspense>
     </AnimatePresence>
   );
 };
