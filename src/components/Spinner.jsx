a2575328b2d5299869da3a9f159638c7ff18b072
Anthony Maras <marasantonis@gmail.com>
2022-12-27 15:29:25 +0200

    clean up

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -1,16 +1,12 @@
+import React from 'react';
 import FadeLoader from 'react-spinners/FadeLoader';
 import style from '../styles/components/Spinner.module.scss';
-import { AnimatePresence } from 'framer-motion';
-
-import React from 'react';
 
 const Spinner = () => {
   return (
-    <AnimatePresence>
-      <div className={style['spinner']}>
-        <FadeLoader color="#da6f41" />
-      </div>
-    </AnimatePresence>
+    <div className={style['spinner']}>
+      <FadeLoader color="#da6f41" />
+    </div>
   );
 };
 
